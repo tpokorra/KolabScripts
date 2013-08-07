4d2fc3a985711664f1873a73072bf3175ecf0aeb
Timotheus Pokorra <tp@tbits.net>
2013-08-07 09:18:11 +0200

    addDomain.php for Kolab 3.0, and postfix init for multiple domains

--- kolab3.0/initMultiDomain.sh
+++ kolab3.0/initMultiDomain.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
+
+#####################################################################################
 #Removing Canonification from Cyrus IMAP
 # TODO: could preserve canonification: http://www.intevation.de/pipermail/kolab-users/2012-August/013747.html
+#####################################################################################
 sed -r -i -e 's/^auth_mech/#auth_mech/g' /etc/imapd.conf
 sed -r -i -e 's/^pts_module/#pts_module/g' /etc/imapd.conf
 sed -r -i -e 's/^ldap_/#ldap_/g' /etc/imapd.conf
 service cyrus-imapd restart
+
+#####################################################################################
+#Update Postfix LDAP Lookup Tables
+# support subdomains too, search_base = dc=%3,dc=%2,dc=%1
+# see http://www.intevation.de/pipermail/kolab-users/2013-January/014270.html
+#####################################################################################
+rm -f /etc/postfix/ldap/*_3.cf
+for f in `find /etc/postfix/ldap/ -type f -name "*.cf" ! -name "mydestination.cf"`;
+do
+  f3=${f/.cf/_3.cf}
+  cp $f $f3
+  sed -r -i -e 's/^search_base = .*$/search_base = dc=%2,dc=%1/g' $f
+  sed -r -i -e 's/^search_base = .*$/search_base = dc=%3,dc=%2,dc=%1/g' $f3
+done
  
+sed -r -i -e 's#^transport_maps = .*$#transport_maps = ldap:/etc/postfix/ldap/transport_maps.cf, ldap:/etc/postfix/ldap/transport_maps_3.cf#g' /etc/postfix/main.cf
+sed -r -i -e 's#^virtual_alias_maps = .*$#virtual_alias_maps = $alias_maps, ldap:/etc/postfix/ldap/virtual_alias_maps.cf, ldap:/etc/postfix/ldap/mailenabled_distgroups.cf, ldap:/etc/postfix/ldap/mailenabled_dynamic_distgroups.cf, ldap:/etc/postfix/ldap/virtual_alias_maps_3.cf, ldap:/etc/postfix/ldap/mailenabled_distgroups_3.cf, ldap:/etc/postfix/ldap/mailenabled_dynamic_distgroups_3.cf#g' /etc/postfix/main.cf
+sed -r -i -e 's#^local_recipient_maps = .*$#local_recipient_maps = ldap:/etc/postfix/ldap/local_recipient_maps.cf, ldap:/etc/postfix/ldap/local_recipient_maps_3.cf#g' /etc/postfix/main.cf
+ 
+service postfix restart
+
+#####################################################################################
+# apply a couple of patches, see related kolab bugzilla number in filename, eg. https://issues.kolab.org/show_bug.cgi?id=2018
+##################################################################################### 
 patch -p1 -d /usr/share/kolab-webadmin < patches/patchMultiDomainAdminsBug2018.patch
 patch -p1 -d /usr/share/kolab-webadmin < patches/mailquotaBug1966.patch
 patch -p1 -d /usr/share/kolab-webadmin < patches/validationOptionalValuesBug2045.patch
