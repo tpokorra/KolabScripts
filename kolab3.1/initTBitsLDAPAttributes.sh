6c9366637d0b800ec973f94a31348d70a7fddf79
Daniel Hoffend <dh@dotlan.net>
2013-11-15 16:05:50 +0100

    Moved TBits attributes into their own schema file, renamed oids, keep in mind OID 12345 sounds unregistered

--- kolab3.1/initTBitsLDAPAttributes.sh
+++ kolab3.1/initTBitsLDAPAttributes.sh
@@ -9,8 +9,9 @@ if [ ! -d patches ]
 then
   mkdir -p patches
   echo Downloading patch patchTBitsLDAPAttributes.patch...
-  wget https://raw.github.com/tpokorra/kolab3_tbits_scripts/master/kolab3.1/patches/patchTBitsLDAPAttributes.patch -O patches/patchTBitsLDAPAttributes.patch
+  wget https://raw.github.com/tpokorra/kolab3_tbits_scripts/master/kolab3.1/patches/99tbits.ldif -O patches/99tbits.ldif
 fi
 
-patch -p0 -i `pwd`/patches/patchTBitsLDAPAttributes.patch
+cp patches/99tbits.ldif /etc/dirsrv/schema/
 
+service dirsrv restart
