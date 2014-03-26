264131a4090c1b3a157df360b1ce57c2a5890662
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-03-26 10:43:02 +0100

    initSSL: move certificates to certs

--- kolab3.1/initSSL.sh
+++ kolab3.1/initSSL.sh
@@ -4,17 +4,22 @@
 # see also https://gist.github.com/dhoffend/7008915 with title: Simple SSL Configuration for Kolab 3.1
 #####################################################################################
 
-echo "Warning: this has not been tested for Debian yet!!!"
-
 export server_name=example.org
 export key_directory=/etc/pki/tls
 export sslgroup=ssl
 export ca_file=startcom-ca.pem
 export ca_subclass_file=startcom-sub.class2.server.ca.pem
 
+if [ -d $key_directory ]
+then
+    # for CentOS: install mod_ssl
+    # yum -y install mod_ssl
+fi
+
 if [ ! -d $key_directory ]
 then
     # Debian: the keys live in a different place
+    echo "Warning: this has not been tested for Debian yet!!!"
     key_directory=/etc/ssl
     sslgroup=ssl-cert
 fi
@@ -32,7 +37,7 @@ answersCreateKey() {
     echo root@localhost.localdomain
 }
 
-if [ ! -f $key_directory/private/$server_name.crt ]
+if [ ! -f $key_directory/certs/$server_name.crt ]
 then
     rm -Rf key
     mkdir key
@@ -42,33 +47,33 @@ then
     answersCreateKey | openssl req -newkey rsa:2048 -keyout $server_name.key -nodes -x509 -days 365 -out $server_name.crt
 
     cp $server_name.key $key_directory/private
-    cp $server_name.crt $key_directory/private
+    cp $server_name.crt $key_directory/certs
 
-    cat $key_directory/private/$server_name.crt \
+    cat $key_directory/certs/$server_name.crt \
         $key_directory/private/$server_name.key \
-        > $key_directory/private/$server_name.bundle.pem
+        > $key_directory/certs/$server_name.bundle.pem
 
-    if [ -f $key_directory/private/$ca_subclass_file ]
+    if [ -f $key_directory/certs/$ca_subclass_file ]
     then
         # using a certificate signed by a CA
-        cat $key_directory/private/$ca_subclass_file \
-            $key_directory/private/$ca_file \
-            >> $key_directory/private/$server_name.bundle.pem
-        cat $key_directory/private/$ca_file \
-            $key_directory/private/$ca_subclass_file \
-            > $key_directory/private/$server_name.ca-chain.pem
+        cat $key_directory/certs/$ca_subclass_file \
+            $key_directory/certs/$ca_file \
+            >> $key_directory/certs/$server_name.bundle.pem
+        cat $key_directory/certs/$ca_file \
+            $key_directory/certs/$ca_subclass_file \
+            > $key_directory/certs/$server_name.ca-chain.pem
     else
         # we do not have a ca for the self signed certificate, so using our own certificate
-        cat $key_directory/private/$server_name.crt > $key_directory/private/$server_name.ca-chain.pem
+        cat $key_directory/certs/$server_name.crt > $key_directory/certs/$server_name.ca-chain.pem
     fi
 
     cd ..
 
     groupadd $sslgroup
-    chmod 640 $key_directory/private/*
-    chown root:$sslgroup $key_directory/private/*
+    chmod -R 640 $key_directory/*
+    chown -R root:$sslgroup $key_directory/*
 
-    # cat $key_directory/private/$ca_file >> $key_directory/certs/ca-bundle.crt
+    # cat $key_directory/certs/$ca_file >> $key_directory/certs/ca-bundle.crt
 fi
 
 #####################################################################################
@@ -76,9 +81,9 @@ fi
 #####################################################################################
 usermod -G saslauth,$sslgroup cyrus
 sed -r -i \
-    -e "s|^tls_cert_file:.*|tls_cert_file: $key_directory/private/$server_name.crt|g" \
+    -e "s|^tls_cert_file:.*|tls_cert_file: $key_directory/certs/$server_name.crt|g" \
     -e "s|^tls_key_file:.*|tls_key_file: $key_directory/private/$server_name.key|g" \
-    -e "s|^tls_ca_file:.*|tls_ca_file: $key_directory/private/$server_name.ca-chain.pem|g" \
+    -e "s|^tls_ca_file:.*|tls_ca_file: $key_directory/certs/$server_name.ca-chain.pem|g" \
     /etc/imapd.conf
 service cyrus-imapd restart
 
@@ -87,8 +92,8 @@ service cyrus-imapd restart
 #####################################################################################
 usermod -G mail,$sslgroup postfix
 postconf -e smtpd_tls_key_file=$key_directory/private/$server_name.key
-postconf -e smtpd_tls_cert_file=$key_directory/private/$server_name.crt
-postconf -e smtpd_tls_CAfile=$key_directory/private/$server_name.ca-chain.pem
+postconf -e smtpd_tls_cert_file=$key_directory/certs/$server_name.crt
+postconf -e smtpd_tls_CAfile=$key_directory/certs/$server_name.ca-chain.pem
 service postfix restart
 
 #####################################################################################
@@ -99,7 +104,7 @@ if [ -d /etc/httpd ]
 then
     #certutil -d /etc/httpd/alias -A  -t "CT,," -n "StartCom Certification Authority" -i $key_directory/private/$ca_file
     pwd=foo
-    openssl pkcs12 -export -in $key_directory/private/$server_name.crt -inkey $key_directory/private/$server_name.key -out /tmp/$server_name.p12 -name Server-Cert -passout pass:$pwd
+    openssl pkcs12 -export -in $key_directory/certs/$server_name.crt -inkey $key_directory/private/$server_name.key -out /tmp/$server_name.p12 -name Server-Cert -passout pass:$pwd
     echo "$pwd" > /tmp/foo
     pk12util -i /tmp/$server_name.p12 -d /etc/httpd/alias -w /tmp/foo -k /dev/null
     rm /tmp/foo
@@ -123,7 +128,7 @@ then
 else
     #certutil -d /etc/apache2/alias -A  -t "CT,," -n "StartCom Certification Authority" -i $key_directory/private/$ca_file
     pwd=foo
-    openssl pkcs12 -export -in $key_directory/private/$server_name.crt -inkey $key_directory/private/$server_name.key -out /tmp/$server_name.p12 -name Server-Cert -passout pass:$pwd
+    openssl pkcs12 -export -in $key_directory/certs/$server_name.crt -inkey $key_directory/private/$server_name.key -out /tmp/$server_name.p12 -name Server-Cert -passout pass:$pwd
     echo "$pwd" > /tmp/foo
     pk12util -i /tmp/$server_name.p12 -d /etc/apache2/alias -w /tmp/foo -k /dev/null
     rm /tmp/foo
