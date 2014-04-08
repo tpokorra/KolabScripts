c6c6f19e1c6fec13c7190dc521dec4d8aff595ab
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-04-08 10:51:25 +0200

    according to the comments on bug 2648, use catchall@mydomain.org as the identifier for catchall aliases. This is the easiest solution because there is no change to email address validation necessary. and the aliases might be used for sender addresses which would cause trouble if the address was just @mydomain.org

--- kolab3.1/initMailCatchall.sh
+++ kolab3.1/initMailCatchall.sh
@@ -7,10 +7,10 @@
 filename=/etc/postfix/ldap/virtual_alias_maps_catchall.cf
 filename_3=/etc/postfix/ldap/virtual_alias_maps_catchall_3.cf
 cp /etc/postfix/ldap/virtual_alias_maps.cf $filename
-sed -i -e 's#^query_filter = .*#query_filter = (\&(alias=@%d)(objectclass=inetorgperson))#g' $filename
+sed -i -e 's#^query_filter = .*#query_filter = (\&(alias=catchall@%d)(objectclass=inetorgperson))#g' $filename
 
 cp /etc/postfix/ldap/virtual_alias_maps_3.cf $filename_3
-sed -i -e 's#^query_filter = .*#query_filter = (\&(alias=@%d)(objectclass=inetorgperson))#g' $filename_3
+sed -i -e 's#^query_filter = .*#query_filter = (\&(alias=catchall@%d)(objectclass=inetorgperson))#g' $filename_3
 
 sed -i -e "s#ldap:/etc/postfix/ldap/virtual_alias_maps.cf#ldap:/etc/postfix/ldap/virtual_alias_maps.cf, ldap:$filename#" /etc/postfix/main.cf
 sed -i -e "s#ldap:/etc/postfix/ldap/virtual_alias_maps_3.cf#ldap:/etc/postfix/ldap/virtual_alias_maps_3.cf, ldap:$filename_3#" /etc/postfix/main.cf
@@ -18,9 +18,3 @@ sed -i -e "s#ldap:/etc/postfix/ldap/virtual_alias_maps_3.cf#ldap:/etc/postfix/ld
 postmap $filename
 postmap $filename_3
 service postfix restart
-
-#####################################################################################
-# apply a couple of patches, see related kolab bugzilla number in filename, eg. https://issues.kolab.org/show_bug.cgi?id=1869
-#####################################################################################
-
-patch -p1 -i `pwd`/patches/allowCatchallAliasBug2648.patch -d /usr/share/kolab-webadmin
