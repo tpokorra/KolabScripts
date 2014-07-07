e28ae8d8235a0f5cf31def85521e62a9d0f0982a
Timotheus Pokorra <tp@tbits.net>
2014-07-07 10:32:50 +0200

    there is no gpg key for the development repo

--- kolab/reinstallCentOS.sh
+++ kolab/reinstallCentOS.sh
@@ -84,6 +84,7 @@ wget $obs/Kolab:/3.2/$OBS_repo_OS/Kolab:3.2.repo -O kolab-3.2.repo
 wget $obs/Kolab:/3.2:/Updates/$OBS_repo_OS/Kolab:3.2:Updates.repo -O kolab-3.2-updates.repo
 wget $obs/Kolab:/Development/$OBS_repo_OS/Kolab:Development.repo -O kolab-3-development.repo
 wget $obs/home:/tpokorra:/branches:/Kolab:/Development/$OBS_repo_OS/home:tpokorra:branches:Kolab:Development.repo -O kolab-3-obs-tpokorra-nightly.repo
+sed -i "s/gpgcheck=1/gpgcheck=0/g" kolab-3-development.repo
 sed -i "s/gpgcheck=1/gpgcheck=0/g" kolab-3-obs-tpokorra-nightly.repo
 cd -
 
