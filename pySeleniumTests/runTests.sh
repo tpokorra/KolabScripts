88c75910b77ff6a3774bc2557c62f3fe0517b086
Timotheus Pokorra <tp@tbits.net>
2014-07-10 16:44:57 +0200

    add some comments to the tests

--- pySeleniumTests/runTests.sh
+++ pySeleniumTests/runTests.sh
@@ -15,16 +15,19 @@ if [[ "$tests" == "all" || "$tests" == "vanilla" ]]; then
   ./testRoundcubeChangePassword.py KolabRoundcubeChangePassword.test_edit_user_password
 fi
 
-# requires configuration for catchall and forwarding
-if [[ "$tests" == "all" || "$tests" == "catchallforwarding" ]]; then 
+# requires configuration for catchall and forwarding, and multidomain
+if [[ "$tests" == "all" || "$tests" == "catchallforwarding" ]]; then
+  # we need the multidomain script and patches installed, because otherwise we need to wait for up to 10 minutes for the domain sync to happen
   ./testEmailCatchAll.py
   ./testEmailForwarding.py
 fi
 
 # requires multi domain patch
 if [[ "$tests" == "all" || "$tests" == "multidomain" ]]; then
+  # these tests have been run in vanilla, but this time we run all test cases
   ./testCreateUserAndEditSelf.py
   ./testRoundcubeChangePassword.py
+
   ./testEmailCatchAllAcrossDomains.py
   ./testAutoCreateFolders.py
   ./testEmailSharedFolders.py
