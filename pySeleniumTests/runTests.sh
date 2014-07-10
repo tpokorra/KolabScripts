88bf42f71b9f2d0feca4fc9ab7a475ee67ee958a
Timotheus Pokorra <tp@tbits.net>
2014-07-10 17:18:08 +0200

    modifications to the tests script

--- pySeleniumTests/runTests.sh
+++ pySeleniumTests/runTests.sh
@@ -13,13 +13,15 @@ rm -f /tmp/output*.html
 if [[ "$tests" == "all" || "$tests" == "vanilla" ]]; then
   ./testCreateUserAndEditSelf.py KolabWAPCreateUserAndEditSelf.test_edit_user_himself
   ./testRoundcubeChangePassword.py KolabRoundcubeChangePassword.test_edit_user_password
+  ./testEmailSendAndReceive.py
 fi
 
 # requires configuration for catchall and forwarding, and multidomain
 if [[ "$tests" == "all" || "$tests" == "catchallforwarding" ]]; then
   # we need the multidomain script and patches installed, because otherwise we need to wait for up to 10 minutes for the domain sync to happen
   ./testEmailCatchAll.py
-  ./testEmailForwarding.py
+  # ignore other test test_mail_forwarding_external because it needs configuration of a domain that can receive email from outside
+  ./testEmailForwarding.py KolabEmailMailForwarding.test_mail_forwarding
 fi
 
 # requires multi domain patch
@@ -41,7 +43,4 @@ if [[ "$tests" == "all" || "$tests" == "domainadmin" ]]; then
   ./testDomainAdminMaxAccounts.py
   ./testDomainAdminOverallQuota.py
 fi
-
-# disabled because it needs configuration of public email server
-#./testEmailSendAndReceive.py
 
