a3c457d254a7acccdc7eb2f3cbb2d5aa26741e2c
Timotheus Pokorra <tp@tbits.net>
2014-07-17 08:10:52 +0200

    small fix, TimeoutException was not known

--- pySeleniumTests/helperKolabWAP.py
+++ pySeleniumTests/helperKolabWAP.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.common.exceptions import TimeoutException
 
 # useful functions for testing kolab-webadmin
 class KolabWAPTestHelpers(unittest.TestCase):
