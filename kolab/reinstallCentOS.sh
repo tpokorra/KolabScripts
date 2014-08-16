73ac02a81d24ba402ef9152f8304effd3fc5d994
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-08-16 07:29:45 +0200

    don't install nightly packages

--- kolab/reinstallCentOS.sh
+++ kolab/reinstallCentOS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # this script will remove Kolab, and DELETE all YOUR data!!!
-# it will reinstall Kolab, from Kolab Nightly builds
+# it will reinstall Kolab, from Kolab 3.3 Updates
 # you can optionally install the patches from TBits, see bottom of script reinstall.sh
 
 #check that dirsrv will have write permissions to /dev/shm
@@ -83,9 +83,7 @@ rm -Rf kolab-*.repo
 wget $obs/Kolab:/3.2/$OBS_repo_OS/Kolab:3.2.repo -O kolab-3.2.repo
 wget $obs/Kolab:/3.2:/Updates/$OBS_repo_OS/Kolab:3.2:Updates.repo -O kolab-3.2-updates.repo
 wget $obs/Kolab:/Development/$OBS_repo_OS/Kolab:Development.repo -O kolab-3-development.repo
-wget $obs/home:/tpokorra:/branches:/Kolab:/Development/$OBS_repo_OS/home:tpokorra:branches:Kolab:Development.repo -O kolab-3-obs-tpokorra-nightly.repo
 sed -i "s/gpgcheck=1/gpgcheck=0/g" kolab-3-development.repo
-sed -i "s/gpgcheck=1/gpgcheck=0/g" kolab-3-obs-tpokorra-nightly.repo
 cd -
 
 # add priority = 0 to kolab repo files
