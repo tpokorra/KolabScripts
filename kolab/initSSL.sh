9d557dfcd6e4239b90263ce6c9232c41cc679b95
Timotheus Pokorra <tp@tbits.net>
2014-09-02 14:12:27 +0200

    fix problems with roundcube assets, with ssl

--- kolab/initSSL.sh
+++ kolab/initSSL.sh
@@ -146,6 +146,15 @@ then
 \tRedirectMatch ^/$ /roundcubemail/\n"
 
       sed -i -e "s#</VirtualHost>#$newConfigLines</VirtualHost>#" /etc/httpd/conf.d/ssl.conf
+
+      newConfigLines="\t\n \
+\t# Be compatible with older packages and installed plugins.\n \
+\tRewriteCond %{REQUEST_URI} ^/roundcubemail/assets/\n \
+\tRewriteCond %{REQUEST_URI} \!-f\n \
+\tRewriteCond %{REQUEST_URI} \!-d\n \
+\tRewriteRule .*/roundcubemail/assets/(.*)\$ /roundcubemail/\$1 [PT,L]\n"
+
+      sed -i -e "s~</VirtualHost>~$newConfigLines</VirtualHost>~" /etc/httpd/conf.d/ssl.conf
     fi
 
     service httpd restart
@@ -175,7 +184,16 @@ else
 \tRewriteRule ^/webmail/[a-f0-9]{16}/(.*) /webmail/\$1 [PT,L]\n \
 \tRedirectMatch ^/$ /roundcubemail/\n"
 
-    sed -i -e "s#</VirtualHost>#$newConfigLines</VirtualHost>#" /etc/apache2/conf.d/nss.conf
+    sed -i -e "s#</VirtualHost>#$newConfigLines</VirtualHost>#" /etc/httpd/conf.d/ssl.conf
+
+    newConfigLines="\t\n \
+\t# Be compatible with older packages and installed plugins.\n \
+\tRewriteCond %{REQUEST_URI} ^/roundcubemail/assets/\n \
+\tRewriteCond %{REQuEST_URI} \!-f\n \
+\tRewriteCond %{REQuEST_URI} \!-d\n \
+\tRewriteRule .*/roundcubemail/assets/(.*)\$ /roundcubemail/\$1 [PT,L]\n"
+
+    sed -i -e "s~</VirtualHost>~$newConfigLines</VirtualHost>~" /etc/apache2/conf.d/nss.conf
 
     service apache2 restart
 fi
