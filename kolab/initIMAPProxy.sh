2b91ded866a294ce96cf2793566979220f2af332
Timotheus Pokorra <tp@tbits.net>
2014-12-15 10:32:30 +0100

    up-imapproxy does not need stunnel, can connect to cyrus via tls

--- kolab/initIMAPProxy.sh
+++ kolab/initIMAPProxy.sh
@@ -3,16 +3,9 @@
 proxy=up-imapproxy
 #proxy=nginx
 
-yum install -y stunnel $proxy
-
-cp imapproxy/stunnel.conf /etc/stunnel/stunnel.conf
-cp imapproxy/stunnel /etc/init.d/stunnel
-chmod a+x /etc/init.d/stunnel
-service stunnel start
-service stunnel on
-
 if [[ "$proxy" -eq "up-imapproxy" ]]
 then
+  yum install -y up-imapproxy
   cp imapproxy/imapproxy.conf /etc/
   service imapproxy start
   chkconfig imapproxy on
@@ -20,6 +13,13 @@ fi
 
 if [[ "$proxy" -eq "nginx" ]]
 then
+  yum install -y stunnel nginx
+  cp imapproxy/stunnel.conf /etc/stunnel/stunnel.conf
+  cp imapproxy/stunnel /etc/init.d/stunnel
+  chmod a+x /etc/init.d/stunnel
+  service stunnel start
+  service stunnel on
+
   cp imapproxy/nginx.conf /etc/nginx/nginx.conf  
   service nginx start
   chkconfig nginx on
