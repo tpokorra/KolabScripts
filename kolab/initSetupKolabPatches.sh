73b8d94c775b7fa4f3218411c1ca4b43bc0e9306
Timotheus Pokorra <tp@tbits.net>
2015-10-27 11:55:07 +0100

    Debian, Apache problems: workaround for bug 2050

--- kolab/initSetupKolabPatches.sh
+++ kolab/initSetupKolabPatches.sh
@@ -21,9 +21,12 @@ patch -p1 -i `pwd`/patches/fixDomainSyncBug5202.patch -d $pythonDistPackages || 
 #echo "applying patch for waiting after restart of dirsrv (necessary on Debian)"
 #patch -p1 -i `pwd`/patches/setupKolabSleepDirSrv.patch -d $pythonDistPackages || exit -1
 
-# TODO on Debian, we need to install the rewrite for the csrf token
-if [ -f /etc/apache2/sites-enabled/000-default ]
+if [[ $OS == Debian* ]]
 then
+      # workaround for bug 2050, https://issues.kolab.org/show_bug.cgi?id=2050
+      echo "export ZEND_DONT_UNLOAD_MODULES=1" >> /etc/apache2/envvars
+
+      # TODO on Debian, we need to install the rewrite for the csrf token
       newConfigLines="\tRewriteEngine On\n \
 \tRewriteRule ^/roundcubemail/[a-f0-9]{16}/(.*) /roundcubemail/\$1 [PT,L]\n \
 \tRewriteRule ^/webmail/[a-f0-9]{16}/(.*) /webmail/\$1 [PT,L]\n \
