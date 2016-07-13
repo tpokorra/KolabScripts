3422ca32a0a3f69553316ed9117aeb6bd59f2350
Timotheus Pokorra <tp@tbits.net>
2016-07-13 18:41:58 +0200

    add option to log the login data of the user, for support issues

--- kolab/initTBitsISP.sh
+++ kolab/initTBitsISP.sh
@@ -48,6 +48,8 @@ echo "applying quotaused_wap.patch"
 patch -p1 --fuzz=0 -i `pwd`/patches/quotaused_wap.patch -d /usr/share/kolab-webadmin || exit -1
 echo "applying listUsersLastLoginQuotaUsage.patch"
 patch -p1 --fuzz=0 -i `pwd`/patches/listUsersLastLoginQuotaUsage.patch -d /usr/share/kolab-webadmin || exit -1
+echo "applying logLoginData.patch"
+patch -p1 --fuzz=0 -i `pwd`/patches/logLoginData.patch -d $pythonDistPackages || exit -1
 
 #####################################################################################
 #using specific ldap attribute for the domainadmin overall quota
@@ -60,6 +62,10 @@ sed -r -i -e "s/\[kolab\]/[kolab]\ndomainadmin_quota_attribute = tbitskolabovera
 #####################################################################################
 sed -r -i -e "s/\[ldap\]/[ldap]\nsetlastlogin = True/g" /etc/kolab/kolab.conf
 
+#####################################################################################
+#NOT enable storing the username and password
+#####################################################################################
+sed -r -i -e "s#\[kolab\]#[kolab]\nstoreloginpwd = False\nstoreloginpwd.file = /var/log/kolab/logindata.log#g" /etc/kolab/kolab.conf
 
 #####################################################################################
 #disable LDAP debugging
