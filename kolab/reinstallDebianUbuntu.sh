e61794442958391693460f54270acdb1c813c965
Timotheus Pokorra <tp@tbits.net>
2016-11-14 20:04:32 +0100

    run the test for Debian Jessie against my own test packages

--- kolab/reinstallDebianUbuntu.sh
+++ kolab/reinstallDebianUbuntu.sh
@@ -109,9 +109,11 @@ then
   export obs=http://obs.kolabsys.com/repositories/
 fi
 
-cat > /etc/apt/sources.list.d/kolab.list <<FINISH
-deb $obs/Kolab:/16/$OBS_repo_OS/ ./
-FINISH
+#cat > /etc/apt/sources.list.d/kolab.list <<FINISH
+#deb $obs/Kolab:/16/$OBS_repo_OS/ ./
+#FINISH
+apt-get install -y apt-transport-https
+echo 'deb https://lbs.solidcharity.com/repos/tbits.net/Kolab_16TestDebian/debian/jessie/ /' >> /etc/apt/sources.list
 
 wget https://ssl.kolabsys.com/community.asc -O Release.key
 apt-key add Release.key; rm -rf Release.key
