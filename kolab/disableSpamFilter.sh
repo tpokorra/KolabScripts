9885e01f482d6718ab8a457e74390ea232ee9e97
Timotheus Pokorra <tp@tbits.net>
2016-12-14 11:27:47 +0100

    add a patch for disabling amavis and clamav this is useful for running a test machine with dply, with only 512 MB of RAM

---
+++ kolab/disableSpamFilter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPTSPATH=`dirname ${BASH_SOURCE[0]}`
+source $SCRIPTSPATH/lib.sh
+
+DetermineOS
+InstallWgetAndPatch
+DeterminePythonPath
+
+#####################################################################################
+# disable the spam filter, if it is not needed
+#####################################################################################
+
+echo "disabling amavis and clamd"
+patch -p1 -i `pwd`/patches/disableSpamFilter.patch -d $pythonDistPackages || exit -1
