1ea314f2ba1378e1be9a2d9f4ef50c83b916b5cb
Timotheus Pokorra <tp@tbits.net>
2017-10-19 16:39:39 +0200

    another fix for the timezone issue Etc/GMT-1 (invalid Olson timezone)

--- kolab/initSetupKolabPatches.sh
+++ kolab/initSetupKolabPatches.sh
@@ -31,6 +31,8 @@ patch -p1 --fuzz=0 -i `pwd`/patches/fixPykolabIMAPKeepAlive.patch -d $pythonDist
 
 echo "apply patch for Etc timezone in roundcube plugins/calendar"
 patch -p1 --fuzz=0 -i `pwd`/patches/roundcube_calendar_etc_timezone_T2666.patch -d /usr/share/roundcubemail || exit -1
+# another way to fix it, in the jstz library (see also https://bitbucket.org/pellepim/jstimezonedetect/issues/168/ignore-timezones-like-etc-gmt-1)
+sed -i 's#"UTC"===a)#"UTC"===a)\&\&a.indexOf("Etc")<0#' /usr/share/roundcubemail/public_html/assets/program/js/jstz.min.js
 
 fi
 
