c2aa54c3150078bbdbeee39f24b42732bc265a28
Timotheus Pokorra <tp@tbits.net>
2018-01-23 16:24:18 +0100

    call helper tear_down in all test cases so that the Xvfb display is closed

--- pySeleniumTests/testEmailCatchAllAcrossDomains.py
+++ pySeleniumTests/testEmailCatchAllAcrossDomains.py
@@ -58,11 +58,7 @@ class KolabEmailCatchAllAcrossDomains(unittest.TestCase):
         kolabWAPhelper.logout_roundcube()
 
     def tearDown(self):
-        
-        # write current page for debugging purposes
-        self.kolabWAPhelper.log_current_page()
-        
-        self.driver.quit()
+        self.kolabWAPhelper.tear_down()
 
 if __name__ == "__main__":
     unittest.main()
--- pySeleniumTests/testEmailForwarding.py
+++ pySeleniumTests/testEmailForwarding.py
@@ -79,11 +79,7 @@ class KolabEmailMailForwarding(unittest.TestCase):
         kolabWAPhelper.logout_roundcube()
 
     def tearDown(self):
-        
-        # write current page for debugging purposes
-        self.kolabWAPhelper.log_current_page()
-        
-        self.driver.quit()
+        self.kolabWAPhelper.tear_down()
 
 if __name__ == "__main__":
     unittest.main()
