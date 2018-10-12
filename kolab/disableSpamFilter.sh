ea9e5d360acb8998e97fd0363a5211d9a607fa8e
LBS BuildBot <tp@tbits.net>
2018-10-12 21:38:44 +0200

    don't apply patches for disabling spamfilter if it has been applied in the package already

--- kolab/disableSpamFilter.sh
+++ kolab/disableSpamFilter.sh
@@ -7,10 +7,18 @@ DetermineOS
 InstallWgetAndPatch
 DeterminePythonPath
 
+if [ -z $APPLYPATCHES ]
+then
+  APPLYPATCHES=1
+fi
+
 #####################################################################################
 # disable the spam filter, if it is not needed
 #####################################################################################
 
-echo "disabling amavis and clamd"
-patch -p1 -i `pwd`/patches/disableSpamFilter.patch -d $pythonDistPackages || exit -1
-patch -p1 -i `pwd`/patches/disableSpamFilter2.patch -d /usr || exit -1
+
+if [ $APPLYPATCHES -eq 1 ]; then
+    echo "disabling amavis and clamd"
+    patch -p1 -i `pwd`/patches/disableSpamFilter.patch -d $pythonDistPackages || exit -1
+    patch -p1 -i `pwd`/patches/disableSpamFilter2.patch -d /usr || exit -1
+fi
